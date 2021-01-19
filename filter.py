ff8aafa06eb0bfadaa60ab5143d94bb50532e7a7
Rozi Szabó <r.e.szabo@students.uu.nl>
2021-01-19 14:16:34 +0100

    Extended filter for csv file

--- filter.py
+++ filter.py
@@ -13,19 +13,26 @@ for dict in data:
     for key in keywords:
         if 'http://purl.org/dc/elements/1.1/description' in dict and key in str(dict["http://purl.org/dc/elements/1.1/description"]).lower() and dict not in reduced_data:
             reduced_data.append(dict.copy()) 
+        if 'ontology/occupation_label' in dict and key in str(dict["ontology/occupation_label"]).lower() and dict not in reduced_data:
+            reduced_data.append(dict.copy()) 
+        if 'ontology/occupation' in dict and key in str(dict["ontology/occupation"]).lower() and dict not in reduced_data:
+            reduced_data.append(dict.copy()) 
+        if 'http://www.w3.org/2000/01/rdf-schema#seeAlso_label' in dict and key in str(dict["http://www.w3.org/2000/01/rdf-schema#seeAlso_label"]).lower() and dict not in reduced_data:
+            reduced_data.append(dict.copy()) 
+
 
 ## Creating subset json file 
 with open('reduced_dataset.json', 'w') as file:
     json.dump(reduced_data, file, indent=1)
 
-## List of directory with only title and description as keys
+## List of directory with the coloumns we want as keys
 music_dict = []
 for dict in reduced_data:
-    x = {'title':dict['title'] ,'description':dict['http://purl.org/dc/elements/1.1/description'],'birth date':dict.get('ontology/birthDate'), 'death date':dict.get('ontology/deathDate'),'cause of death':dict['ontology/deathCause_label']}
+    x = {'title':dict['title'] ,'description':dict.get('http://purl.org/dc/elements/1.1/description'),'occupation':dict.get('ontology/occupation_label'),'birth date':dict.get('ontology/birthDate'), 'death date':dict.get('ontology/deathDate'),'cause of death':dict['ontology/deathCause_label']}
     music_dict.append(x)
 
 ## Creating csv file
 with open('music.csv', 'w', encoding="utf-8") as file:
-    file.write("title,description,birth date,death date,cause of death\n")
+    file.write("title,description,occupation,birth date,death date,cause of death\n")
     for person in music_dict:
-        file.write(f"{person['title']},{person['description']},{person['birth date']},{person['death date']},{person['cause of death']}\n")
+        file.write(f"{person['title']},{person['description']},{person['occupation']},{person['birth date']},{person['death date']},{person['cause of death']}\n")
