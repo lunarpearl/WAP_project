01062b2aeb4b0b1439072db1c29266d4153d8342
Rozi Szabó <r.e.szabo@students.uu.nl>
2021-01-19 16:15:40 +0100

    Added comments

--- ageatdeath_compare.R
+++ ageatdeath_compare.R
@@ -1,8 +1,13 @@
 
 library(tidyverse)
 
+## Load in csv as dataframe
 df <- read_csv2('death_genre.csv')
 
+## Transform brith and death dates to date formats 
+## -> creates NA for dates which are lists
+## + calculate age at death
+## + add logical expression coloumn if the genre contains rock or not
 calculations_df <- df %>%
   mutate(`birth date` = as.Date(`birth date`),
          `death date` = as.Date(`death date`),
@@ -10,6 +15,7 @@ calculations_df <- df %>%
          age_at_death = days_at_death / 365.25,
          is_rock = str_detect(genre,coll("rock", ignore_case = TRUE)))
 
+## Group by the logical expression then calculate mean age for is_rock TRUE and is_rock FALSE
 summary_df <- drop_na(calculations_df) %>%
   select(age_at_death, is_rock) %>%
   group_by(is_rock) %>%
