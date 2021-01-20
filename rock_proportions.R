da7da3c8576cfde6122f29e80d26aa31d51253e5
Meru Nurberdikhanova <nurberdikhanova.aktau@gmail.com>
2021-01-20 12:17:25 +0100

    rock in all and in dead musicians 1

---
+++ rock_proportions.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+allmusicians_genre <- read_csv2('allmusic_genre.csv')
+#making a subset that only includes all musicians that have 'rock' in their genres
+allrock <- allmusicians_genre[grep('rock',allmusicians_genre$genre), ]
+#calculate number of rock musicians within all musicians
+number_allmusicians <- nrow(allmusicians_genre)
+number_rock <- nrow(allrock)
+proportion_rock_inall <- number_rock/number_allmusicians*100
+#now we look at only musicians that passed away
+deadmusicians_genre <- read_csv2('death_genre.csv')
+#subset of dead rock musicians
+deadrock <- deadmusicians_genre[grep('rock',deadmusicians_genre$genre), ]
+#calculate number of rock musicians that passed away
+number_deadmusicians <- nrow(deadmusicians_genre)
+number_deadrock <- nrow(deadrock)
+proportion_rock_indead <- number_deadrock/number_deadmusicians*100
