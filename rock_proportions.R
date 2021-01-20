0eb8f26851600e4d8ed858a902c49e0e6d94c6eb
Rozi Szabó <r.e.szabo@students.uu.nl>
2021-01-20 14:59:13 +0100

    added ignore.case = TRUE

--- rock_proportions.R
+++ rock_proportions.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 allmusicians_genre <- read_csv2('allmusic_genre.csv')
 #making a subset that only includes all musicians that have 'rock' in their genres
-allrock <- allmusicians_genre[grep('rock',allmusicians_genre$genre), ]
+allrock <- allmusicians_genre[grep('rock',allmusicians_genre$genre), ignore.case = TRUE]
 #calculate number of rock musicians within all musicians
 number_allmusicians <- nrow(allmusicians_genre)
 number_rock <- nrow(allrock)
@@ -10,7 +10,7 @@ proportion_rock_inall <- number_rock/number_allmusicians*100
 #now we look at only musicians that passed away
 deadmusicians_genre <- read_csv2('death_genre.csv')
 #subset of dead rock musicians
-deadrock <- deadmusicians_genre[grep('rock',deadmusicians_genre$genre), ]
+deadrock <- deadmusicians_genre[grep('rock',deadmusicians_genre$genre,ignore.case = TRUE), ]
 #calculate number of rock musicians that passed away
 number_deadmusicians <- nrow(deadmusicians_genre)
 number_deadrock <- nrow(deadrock)
